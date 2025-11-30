7c3de6d0f49cbd1dea60e3c268ac9c3925135db3
Nahid Home <2213181@brunel.ac.uk>
2025-11-30 14:31:07 +0600

    add pinia using- npm install pinia @pinia/nuxt

---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="p-5">
+    <h1 class="text-2xl mb-4">Counter: {{ counter.count }}</h1>
+    <button @click="counter.increment" class="px-4 py-2 bg-blue-500 text-white rounded">
+      Increment
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useCounterStore } from '~/stores/counter'
+
+// Initialize the store
+const counter = useCounterStore()
+</script>

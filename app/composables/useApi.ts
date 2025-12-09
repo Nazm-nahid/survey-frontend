bab751501df7f00179b777538452f7d227fc46fb
nazmul.nahid <nazmul.nahid@pathao.com>
2025-12-10 02:20:46 +0600

    fix logout problem

--- app/composables/useApi.ts
+++ app/composables/useApi.ts
@@ -1,17 +1,15 @@
 import axios from "axios";
-import { useAuthStore } from "~/stores/auth";
 
 export const useApi = () => {
-  const auth = useAuthStore();
-
   const api = axios.create({
     baseURL: "http://localhost:3001",
     headers: { "Content-Type": "application/json" },
   });
 
   api.interceptors.request.use((config) => {
-    if (auth.token) {
-      config.headers.Authorization = `Bearer ${auth.token}`;
+    const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     } else {
          navigateTo("/login"); 
     }
